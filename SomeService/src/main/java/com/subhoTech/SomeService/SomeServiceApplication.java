124f3f0b8c4e4272c1adbd8e90039ae34fe651a6
subho <subha130394@gmail.com>
2020-05-26 09:10:51 -0700

    some service is created

---
+++ SomeService/src/main/java/com/subhoTech/SomeService/SomeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.subhoTech.SomeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeServiceApplication.class, args);
+	}
+
+}
